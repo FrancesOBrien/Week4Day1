6b42c0c54676301a46cd2c54450842c67aa2d9dd
FrancesOBrien <114510060+FrancesOBrien@users.noreply.github.com>
2022-10-25 13:34:46 -0400

    get even complete

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ const logNumbers = () => {
     }
 }
 logNumbers()
-console.log("easy going complete")
-
 
 ////////////////////////////////
 // Get Even
 ////////////////////////////////
 
+const getEven = () => {
+    for(let i = 0; i <= 200; i++) {
+        if (i % 2 == 0){
+            console.log(i)
+        }
+    }
+}
+getEven()
 
 ////////////////////////////////
 // Fizz Buzz
