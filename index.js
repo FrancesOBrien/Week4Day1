67317d21c16cc7f4430b0d6851f00b761f778459
FrancesOBrien <114510060+FrancesOBrien@users.noreply.github.com>
2022-10-25 13:42:14 -0400

    Fizz Buzz complete

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ getEven()
 // Fizz Buzz
 ////////////////////////////////
 
+const getMyNumbers = () => {
+    for(let i = 0; i <= 100; i++){
+        if (i % 3 == 0 && i % 5 == 0){
+            console.log(i + " FizzBuzz");
+        }else if(i % 3 == 0){
+            console.log(i + " Fizz");
+        } else if (i % 5 == 0){
+            console.log(i + " Buzz");
+        } else console.log(i)
+    }
+}
+getMyNumbers()
 
 ////////////////////////////////
 // Wild Wild Life
